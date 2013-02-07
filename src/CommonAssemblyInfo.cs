ddcaf908165f1db03706b54e878a059c5d893dbb
Adam Ralph <adam@adamralph.com>
2013-02-07 22:49:43 +0100

    added documentation for Intellisense incremented to 0.2.0 for release

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -12,8 +12,8 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyProduct("LiteGuard")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyInformationalVersion("0.2.0")]
 [assembly: SuppressMessage(
     "Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly", Justification = "Using SemVer in AssemblyInformationalVersion.")]
