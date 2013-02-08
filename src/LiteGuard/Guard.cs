c0ce6257a639d3061fe2767a053dbfd4dabda53b
Adam Ralph <adam@adamralph.com>
2013-02-08 12:56:55 +0100

    #2 added AgainstNullArgumentIfNullable

--- src/LiteGuard/Guard.cs
+++ src/LiteGuard/Guard.cs
@@ -19,11 +19,27 @@ namespace LiteGuard
         /// <typeparam name="TArgument">The type of the argument.</typeparam>
         /// <param name="parameterName">Name of the parameter.</param>
         /// <param name="argument">The argument.</param>
-        /// <exception cref="System.ArgumentNullException"><paramref name="argument"/> is <c>null</c>.</exception>
+        /// <exception cref="System.ArgumentNullException"><paramref name="argument" /> is <c>null</c>.</exception>
         [DebuggerStepThrough]
         public static void AgainstNullArgument<TArgument>(string parameterName, [ValidatedNotNull]TArgument argument) where TArgument : class
         {
             if (argument == null)
+            {
+                throw new ArgumentNullException(parameterName, string.Format(CultureInfo.InvariantCulture, "{0} is null.", parameterName));
+            }
+        }
+
+        /// <summary>
+        /// Guards against a null argument if <typeparamref name="TArgument" /> is a nullable type.
+        /// </summary>
+        /// <typeparam name="TArgument">The type of the argument.</typeparam>
+        /// <param name="parameterName">Name of the parameter.</param>
+        /// <param name="argument">The argument.</param>
+        /// <exception cref="System.ArgumentNullException"><paramref name="argument" /> is <c>null</c>.</exception>
+        [DebuggerStepThrough]
+        public static void AgainstNullArgumentIfNullable<TArgument>(string parameterName, [ValidatedNotNull]TArgument argument)
+        {
+            if (typeof(TArgument).IsNullableType() && argument == null)
             {
                 throw new ArgumentNullException(parameterName, string.Format(CultureInfo.InvariantCulture, "{0} is null.", parameterName));
             }
@@ -36,15 +52,24 @@ namespace LiteGuard
         /// <param name="parameterName">Name of the parameter.</param>
         /// <param name="propertyName">Name of the property.</param>
         /// <param name="argumentProperty">The argument property.</param>
-        /// <exception cref="System.ArgumentException"><paramref name="argumentProperty"/> is <c>null</c>.</exception>
+        /// <exception cref="System.ArgumentException"><paramref name="argumentProperty" /> is <c>null</c>.</exception>
         [DebuggerStepThrough]
-        public static void AgainstNullArgumentProperty<TProperty>(string parameterName, string propertyName, [ValidatedNotNull]TProperty argumentProperty)
-            where TProperty : class
+        public static void AgainstNullArgumentProperty<TProperty>(string parameterName, string propertyName, [ValidatedNotNull]TProperty argumentProperty) where TProperty : class
         {
             if (argumentProperty == null)
             {
                 throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, "{0}.{1} is null.", parameterName, propertyName), parameterName);
             }
+        }
+
+        private static bool IsNullableType(this Type type)
+        {
+            return !type.IsValueType || type.IsNullableValueType();
+        }
+
+        private static bool IsNullableValueType(this Type type)
+        {
+            return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
         }
 
         // NOTE: when applied to a parameter, this attribute provides an indication to code analysis that the argument has been null checked
