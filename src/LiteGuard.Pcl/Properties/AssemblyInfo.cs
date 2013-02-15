1fadbb6cbf69a55d7d9e887b05d9673729b2de5f
Adam Ralph <adam@adamralph.com>
2013-02-15 16:59:55 +0100

    #9 added PCL project and added to nuspec

---
+++ src/LiteGuard.Pcl/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="AssemblyInfo.cs" company="LiteGuard contributors">
+//  Copyright (c) LiteGuard contributors. All rights reserved.
+// </copyright>
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LiteGuard")]
+[assembly: AssemblyDescription("")]
+
+[assembly: CLSCompliant(true)]
