88f0ccff917858f1f73b3c51a7460cbeba1efbaa
Adam Ralph <adam@adamralph.com>
2014-12-14 12:31:18 +0100

    added universal support

--- src/LiteGuard.net35/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿// <copyright file="AssemblyInfo.cs" company="LiteGuard contributors">
-//  Copyright (c) LiteGuard contributors. All rights reserved.
-// </copyright>
-
-#if NET35 || SILVERLIGHT || NETFX_CORE
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-#endif
